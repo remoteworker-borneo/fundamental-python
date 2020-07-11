74b0be4c44d2030cb3a0d8ca1273d6f636024e3c
coderot7 <mr.heeroe@gmail.com>
2020-07-12 06:04:10 +0800

    - tipe data list dengan cara sederhana dan cara ribet :D

--- fundamental1-type-data-penting.py
+++ fundamental1-type-data-penting.py
@@ -8,3 +8,20 @@ print(anak1)
 print(anak2)
 print(anak3)
 print(anak4)
+
+print('\ntipe data list/daftar/array')
+anak = ['eko', 'dwi', 'tri', 'catur']
+print(anak)
+anak.append('limo')
+print(anak)
+
+print('\nsapa anak ke-2')
+print(f'hai {anak[1]}!')
+
+print('\nsapa semua anak')
+for a in anak:
+    print(f'hai {a}!')
+
+print('\nsapa semua anak: cara ribet')
+for a in range(0,len(anak)):
+    print(f'{a+1}. hai {anak[a]}')
